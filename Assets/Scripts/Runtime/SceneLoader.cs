901a5bd97a7a3441e0bb7cf58904689ed6c5a279
Charles <charles.murphy.042@gmail.com>
2024-03-17 12:48:18 -0400

    New enemy events

---
+++ Assets/Scripts/Runtime/SceneLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace kc.runtime.Assets.Scripts.Runtime
+{
+    public class SceneLoader : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void LoadGame()
+        {
+
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+}
